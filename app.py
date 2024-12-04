bb90084f9f061f2bd96ae2654c52b9af65c14856
dankeschun <brnrdfabian@gmail.com>
2024-12-04 19:33:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def recognize():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
